a3c08d5d03fadaafcd4940c3f9f1bd19ba23eb40
Rayan Wilbert <rayan.rmw@gmail.com>
2021-06-29 20:55:08 -0300

    About and services parts done

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,14 @@ document.querySelector("#header nav .icon-menu").addEventListener("click", menu.
 document.querySelector("#header nav .icon-close").addEventListener("click", menu.close)
 links.forEach(link => link.addEventListener("click", menu.close));
 
+
+/* --------------------------    ADD SHADOW IN HEADER    -------------------------- */
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;  // Get height of an element
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= navHeight) {
+        header.classList.add("scroll")
+    }   else {
+        header.classList.remove("scroll");
+    }
+})
