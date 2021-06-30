a94d5e2a1c5a75c984076049b45e7be333c3cd97
Rayan Wilbert <rayan.rmw@gmail.com>
2021-06-30 17:44:57 -0300

    Testimonials and contact sections done

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,30 @@ window.addEventListener('scroll', () => {
         header.classList.remove("scroll");
     }
 })
+
+
+/* --------------------------    TESTIMONIALS SLIDER    -------------------------- */
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+
+  });
+
+
+/* --------------------------    SCROLL REVEAL    -------------------------- */
+const scrollReveal = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duration: 700,
+    reset: true
+});
+scrollReveal.reveal(`#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links`, {interval: 100})
+
